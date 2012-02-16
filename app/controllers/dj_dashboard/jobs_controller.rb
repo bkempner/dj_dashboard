ae3307ecfe46439f83c9066c3b9902e5a51dff79
Ben Kempner <ben@verticalbrands.com>
2012-02-15 17:43:09 -0800

    basic pagination working

--- app/controllers/dj_dashboard/jobs_controller.rb
+++ app/controllers/dj_dashboard/jobs_controller.rb
@@ -4,12 +4,18 @@ module DjDashboard
 
     before_filter :authenticate!
 
+    PAGE_SIZE = 10
+
     def index
       @jobs = Job.fetch
     end
 
     def stats
-      @jobs = Delayed::Job.send(params[:type].to_sym, params[:job_name]).page(params[:page]).per_page(10)
+      @current_page = params[:page] && params[:page].to_i || 1
+      offset = params[:page] && ((params[:page].to_i-1) * PAGE_SIZE) || 0
+      @jobs = Delayed::Job.send(params[:type].to_sym, params[:job_name]).order("created_at desc").limit(PAGE_SIZE).offset(offset)
+      @pages = (1..(Delayed::Job.send(params[:type].to_sym, params[:job_name]).count.to_f / PAGE_SIZE).ceil).to_a
+      @jobs_wp = Delayed::Job.send(params[:type].to_sym, params[:job_name]).page(params[:page]).per_page(10)
       @is_pagination_link = params[:page] ? true : false
       @job_name = @jobs.first.job_name if @jobs.any?
       @stat_type = params[:type]
