45d24e91e4e9b382918cddcaf686ca96b4fa0224
Ben Kempner <ben@verticalbrands.com>
2012-02-16 06:52:44 -0800

    pagination almost done

--- app/controllers/dj_dashboard/jobs_controller.rb
+++ app/controllers/dj_dashboard/jobs_controller.rb
@@ -15,7 +15,7 @@ module DjDashboard
       offset = params[:page] && ((params[:page].to_i-1) * PAGE_SIZE) || 0
       @jobs = Delayed::Job.send(params[:type].to_sym, params[:job_name]).order("created_at desc").limit(PAGE_SIZE).offset(offset)
       @pages = (1..(Delayed::Job.send(params[:type].to_sym, params[:job_name]).count.to_f / PAGE_SIZE).ceil).to_a
-      @jobs_wp = Delayed::Job.send(params[:type].to_sym, params[:job_name]).page(params[:page]).per_page(10)
+      @jobs_wp = Delayed::Job.send(params[:type].to_sym, params[:job_name]).page(params[:page]).per_page(PAGE_SIZE)
       @is_pagination_link = params[:page] ? true : false
       @job_name = @jobs.first.job_name if @jobs.any?
       @stat_type = params[:type]
